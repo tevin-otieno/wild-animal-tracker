2a11d5ad9d3db20120fcb6712c55eb8dfaf0c4e1
tevin-otieno <tevin.otieno@student.moringaschool.com>
2021-11-07 11:21:05 +0300

    credentials to access the data base

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class DB {
+import org.sql2o.Sql2o;
+
+public class DB {
+        //use the line below if you want to run locally replacing user with db username and pass with db password
+        public static Sql2o sql2o=new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "moringa", "pass123");
 }
